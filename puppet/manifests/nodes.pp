0d2a8ed100c1183f77a50f710fce708cabac6b82
Billie Alice Thompson <billie@billiecodes.com>
2013-12-15 15:54:36 +0000

    Added php

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,3 +1,7 @@
 node default {
-hiera_include('classes')
+  hiera_include('classes')
+  $php_ini_instances = hiera('php_ini', [])
+  create_resources('php::ini',$php_ini_instances)
+  $php_modules = hiera('php_modules', [])
+  create_resources('php::module',$php_modules)
 }
